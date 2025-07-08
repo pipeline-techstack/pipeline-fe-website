fedc23b3fbe41efc683afac03d291ded16820cd5
soumaliTS <soumali@techstack.management>
2025-07-08 10:59:27 +0530

    (setup): Setting up nextjs, tailwind shadcn

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button>Primary button colour</Button>
+      <Button variant="secondary">Secondary button colour</Button>
+    </div>
+  );
+}
