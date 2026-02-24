7cfed496c75d02303f8d189e171afaac14d8bfc4
AniketB <aniket@addpipeline.ai>
2026-02-24 15:51:50 +0530

    (refactor): Search params fix in demo page

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,41 +1,26 @@
-"use client";
-
-import { useSearchParams } from "next/navigation";
 import DemoPage from "./demo-page-component";
-import { useEffect, useState } from "react";
 
-function Page() {
-  const params = useSearchParams().get("page") !;
-  const [data, setData] = useState({
-    cta: "",
-    headline: "",
-    page: "",
-  });
+export default function Page({
+  searchParams,
+}: {
+  searchParams: { page?: string };
+}) {
+  const page = searchParams.page ?? "outbound";
 
-  // console.log(params);
-  useEffect(() => {
-    if (params === "revenue") {
-      setData({
-        cta: "Revenue Intelligence Platform",
-        headline:
-          "Understand what's driving the number, and where to focus next",
-        page: params,
-      });
-    } else {
-      setData({
-        cta: "Outbound Intelligence Platform",
-        headline:
-          "LinkedIn Outreach That Delivers High-Intent B2B Conversations",
-        page: params,
-      });
-    }
-  }, []);
+  const data =
+    page === "revenue"
+      ? {
+          cta: "Revenue Intelligence Platform",
+          headline:
+            "Understand what's driving the number, and where to focus next",
+          page: page,
+        }
+      : {
+          cta: "Outbound Intelligence Platform",
+          headline:
+            "LinkedIn Outreach That Delivers High-Intent B2B Conversations",
+          page: page,
+        };
 
-  return (
-    <>
-      <DemoPage {...data} />
-    </>
-  );
+  return <DemoPage {...data} />;
 }
-
-export default Page;
